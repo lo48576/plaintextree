ce2dba94d786a6ed2673ff8a9089563b57980374
YOSHIOKA Takuma <lo48576@hard-wi.red>
2020-01-19 03:17:49 +0900

    Add simple examples to crate-level doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,80 @@
 //! Tree printer.
+//!
+//! # Examples
+//!
+//! ```
+//! use plaintextree::{EdgeConfig, ItemStyle, TreeConfig, TreePrinter};
+//!
+//! let mut buf = ".\n".to_owned();
+//! // You can pass `&mut String` as the inner writer.
+//! let mut printer = TreePrinter::new(&mut buf, TreeConfig::new());
+//! let edge = EdgeConfig::Ascii;
+//!
+//! printer.open_node(ItemStyle::non_last(edge.clone()), "foo")?;
+//! printer.open_node(ItemStyle::non_last(edge.clone()), "bar")?;
+//! printer.open_node(ItemStyle::last(edge.clone()), "baz\n\nmultiline support!")?;
+//! printer.close_node()?; // baz
+//! printer.close_node()?; // bar
+//! printer.open_node(ItemStyle::last(edge.clone()), "qux")?;
+//! printer.open_node(ItemStyle::last(edge.clone()), "quux")?;
+//! printer.close_node()?; // quux
+//! printer.close_node()?; // qux
+//! printer.close_node()?; // foo
+//! printer.open_node(ItemStyle::non_last(edge.clone()), "corge\n")?;
+//! printer.close_node()?; // corge
+//! printer.open_node(ItemStyle::last(edge.clone()), "grault")?;
+//! printer.close_node()?; // grault
+//!
+//! // `finalize()` returns `Result<&mut String, _>` here.
+//! let _ = printer.finalize()?;
+//!
+//! let expected = "\
+//!                 .\n\
+//!                 |-- foo\n\
+//!                 |   |-- bar\n\
+//!                 |   |   `-- baz\n\
+//!                 |   |\n\
+//!                 |   |       multiline support!\n\
+//!                 |   `-- qux\n\
+//!                 |       `-- quux\n\
+//!                 |-- corge\n\
+//!                 `-- grault\n";
+//!
+//! assert_eq!(buf, expected);
+//! # Ok::<_, plaintextree::Error>(())
+//! ```
+//!
+//! Unicode ruled line characters can also be used.
+//!
+//! ```
+//! use plaintextree::{EdgeConfig, ItemStyle, TreeConfig, TreePrinter};
+//!
+//! // You can pass `String` directly as the inner writer.
+//! let mut printer = TreePrinter::new(".\n".to_owned(), TreeConfig::new());
+//! let edge = EdgeConfig::UnicodeSingleWidth;
+//!
+//! printer.open_node(ItemStyle::non_last(edge.clone()), "foo")?;
+//! printer.open_node(ItemStyle::non_last(edge.clone()), "bar")?;
+//! printer.close_node()?; // bar
+//! printer.open_node(ItemStyle::last(edge.clone()), "baz")?;
+//! printer.close_node()?; // baz
+//! printer.close_node()?; // foo
+//! printer.open_node(ItemStyle::last(edge.clone()), "qux")?;
+//! printer.close_node()?; // qux
+//!
+//! // `finalize()` returns `Result<String, _>` here.
+//! let got = printer.finalize()?;
+//!
+//! let expected = "\
+//!                 .\n\
+//!                 ├── foo\n\
+//!                 │   ├── bar\n\
+//!                 │   └── baz\n\
+//!                 └── qux\n";
+//!
+//! assert_eq!(got, expected);
+//! # Ok::<_, plaintextree::Error>(())
+//! ```
 #![forbid(unsafe_code)]
 #![warn(missing_docs)]
 #![warn(clippy::missing_docs_in_private_items)]
